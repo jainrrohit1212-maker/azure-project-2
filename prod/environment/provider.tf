3923517fd1feb29401cc161b90bd18c96e791392
Rohit Jain <jainrrohit1212@gmail.com>
2026-08-16 02:36:23 +0530

    fix terraform types for tflint

--- prod/environment/provider.tf
+++ prod/environment/provider.tf
@@ -1,12 +1,11 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.70.0"
     }
   }
-}
-provider "azurerm" {
-  features {}
 }
 
